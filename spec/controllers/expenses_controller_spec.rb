e5171858a8a57b53d06b93971cd68f94ba67a23e
Piet Schijven <aphexedpiet@gmail.com>
2014-07-11 16:40:10 +0200

    Fixed basic rspec setup

---
+++ spec/controllers/expenses_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe ExpensesController do
+  describe 'GET #index' do
+    it 'provides a list of all the expenses'
+    it 'renders the :index view'
+  end
+#  
+#  describe 'GET #show' do
+#    it 'assigns the requested expense to @expense'
+#    it 'renders the :show view'
+#  end
+#  
+#  describe 'GET #new' do
+#    it 'assigns a new expense to @expense'
+#    it 'renders the :new view'
+#  end
+end
