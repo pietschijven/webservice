9441d33214b68d71375aae15125eadcac17e7338
Piet Schijven <aphexedpiet@gmail.com>
2014-07-15 17:06:52 +0200

    Added validation for user and paid_for_user columns

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,16 @@
+class ExpensesValidator < ActiveModel::Validator
+  def validate(record)
+    if record.user_id == record.paid_for_user_id
+      record.errors[:base] << "Paid_for_user cannot be equal to user"
+    end
+  end
+end
+
 class Expense < ActiveRecord::Base
   belongs_to :user, :class_name => 'User', :foreign_key => 'user_id'
   belongs_to :paid_for_user, :class_name => 'User', :foreign_key => 'paid_for_user_id'
+  
+  validates_with ExpensesValidator
   
   def paid_for_user_name
     expense = Expense.find(self.id)
