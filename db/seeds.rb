26ffdd4dbb7a0356369974e0b643cc43f8abfa31
Piet Schijven <aphexedpiet@gmail.com>
2014-10-06 13:04:58 +0200

    Some temporary frontend stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user_id_list = User.all.map {|x| x.id}
+user_id_list = User.pluck :id
+
 month_list = ["January, 2014", "February, 2014", "March, 2014", "April, 2014", "May, 2014", "June, 2014", "July, 2014", "August, 2014", "September, 2014", "October, 2014", "November, 2014", "December, 2014"].map {|x| Date.parse x}
 
-expenses_list = []
-
-100.times do |x|
-  expenses_list << {:cost => rand(500)/10.0, :user_id => user_id_list[rand(2)], :time_period => month_list[rand(12)]}
-end
-
-expenses_list.each do |expense|
+100.times do
+  expense = {:cost => rand(500)/10.0, :user_id => user_id_list[rand(2)], :time_period => month_list[rand(12)]}
   Expense.create expense
 end
